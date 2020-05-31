bc37e648b5a90f61604eccf1f3f1bb6ffcff8325
tajeddine <tajeddine-dev@hotmail.com>
2020-05-31 19:23:55 +0100

    removing cursor delay animation

--- layouts/default.vue
+++ layouts/default.vue
@@ -101,7 +101,7 @@ export default {
     document.addEventListener('mousemove', e => {
       cursor.setAttribute(
         'style',
-        'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px;'
+        'top: ' + (e.pageY - 0) + 'px; left: ' + (e.pageX - 0) + 'px;'
       )
     })
 
