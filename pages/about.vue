5cecfbc9ccba5b92117d88cf23a0c4619394fc49
tajeddine-alaoui <tajeddine-dev@hotmail.com>
2020-12-26 10:33:10 +0100

    fix typo on about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -49,11 +49,7 @@
 export default {
   head() {
     return {
-<<<<<<< HEAD
       title: 'About.js',
-=======
-      title: 'About',
->>>>>>> dec49bd39beff7965c1528da03881df30a262596
       meta: [
         {
           hid: 'description',
